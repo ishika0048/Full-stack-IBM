34aa673caf9257326133480ddb7d40b2eb6eb5b4
ishika0048 <ishikakarmakar0601@gmail.com>
2025-02-24 21:41:37 +0530

    day 9

---
+++ Day-10/classwork/index.js
@@ -0,0 +1,64 @@
+let str ="sv universcity"
+
+
+console.log(str.length)
+
+console.log(str[-1])
+
+str[0]="R"
+console.log(str)
+let arr=[1,2,3,"ishika",true,null]
+console.log(arr[3])
+arr[3]="Rupsa"
+
+console.log(arr)
+
+
+console.log(arr[arr,length-1])
+arr[4]="Ranit"
+ console.log(arr)
+   
+
+
+
+ //object
+
+ //object literal
+ //new key word
+ //constructor
+
+ let svuclass =["praveen","B Tech",3,"ranit","Btech",3]
+
+ const obj = {
+    Praveen:{class:"B Tech",year: 3,RN:84,subject:["DBMS","NM","CN","FS"]},
+     Ranit:{class : "B Tech",year: 3,RN:84,subject:["DBMS","NM","CN","FS"]},
+     1:"svu"
+
+
+ } 
+
+ obj["1"]="sudarshan"
+
+ console.log(obj)
+
+ delete obj["praveen"]
+
+ console.log(obj)
+
+  obj.Virat = {class:"ICC",year:20,RN:41,subject:["DBMS","NM","cn","FS"]}
+
+  console.log(obj)
+
+  obj.Virat = {class:"ICC",year:20,RN:41,subject:["DBMS","NM","cn","FS"]}
+
+  console.log(obj)
+
+
+
+
+  let name = "Preet"
+  let class="BCCI
+  let sub="FS"
+
+  
+
